bug update for cost calculation

src/gradient_flows/binary_classification.py
@@ -51,9 +51,10 @@ class GradientFlowBinaryClassification(GradientFlowBase):
                 rhs=particles,
             )
         )  # \phi(k(X, Z) @ k(Z, Z)^{-1} U(t)) of size (N, P)
-        return -self.y_train[:, None] * torch.log(prediction) - (
-            1 - self.y_train[:, None]
-        ) * torch.log(1 - prediction)
+        return (
+            -self.y_train[:, None] * torch.log(prediction)
+            - (1 - self.y_train[:, None]) * torch.log(1 - prediction)
+        ).sum(dim=0)
 
     def _calculate_cost_derivative(
         self,

src/gradient_flows/eigenvalue_regression.py
@@ -73,7 +73,7 @@ class GradientFlowEigenvalueRegression(GradientFlowBase):
         return (1 / (2 * self.observation_noise)) * torch.square(
             self.base_gram_induce_train.T @ self.scaled_eigenvectors @ particles
             - self.y_train[:, None]
-        )
+        ).sum(dim=0)
 
     def calculate_energy_potential(self, particles: torch.Tensor) -> float:
         """

src/gradient_flows/regression.py
@@ -48,7 +48,7 @@ class GradientFlowRegression(GradientFlowBase):
         return (1 / (2 * self.observation_noise)) * torch.square(
             self.base_gram_induce_train.T @ inverse_base_gram_particle_vector
             - self.y_train[:, None]
-        )
+        ).sum(dim=0)
 
     def _calculate_cost_derivative(
         self,