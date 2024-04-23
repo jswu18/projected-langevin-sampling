74ea95e5b8edd80aed6d47c2bb2e2bf4353b1a57
James Wu <jswu18@gmail.com>
2024-04-23 06:23:25 -0400

    add mockers for basis and cost

---
+++ mockers/basis.py
@@ -0,0 +1,96 @@
+import torch
+
+from src.projected_langevin_sampling.basis.base import PLSBasis
+
+
+class MockBasis(PLSBasis):
+    """
+    A mock basis used for testing projected Langevin sampling.
+
+    N is the number of training points.
+    M is the dimensionality of the function space approximation (fixed to 10 for this mock basis).
+    J is the number of particles.
+    D is the dimensionality of the data.
+    """
+
+    def approximation_dimension(self) -> int:
+        return 10
+
+    def initialise_particles(
+        self,
+        number_of_particles: int,
+        noise_only: bool = True,
+        seed: Optional[int] = None,
+    ) -> torch.Tensor:
+        """
+        Initialises the particles for the projected Langevin sampling.
+        :param number_of_particles: The number of particles to initialise.
+        :param noise_only: Whether to initialise the particles with noise only.
+        :param seed: An optional seed for reproducibility.
+        :return: A tensor of size (M, J).
+        """
+        return torch.zeros((self.approximation_dimension, number_of_particles))
+
+    def calculate_untransformed_train_prediction_samples(
+        self, particles: torch.Tensor
+    ) -> torch.Tensor:
+        """
+        Calculates the untransformed samples of the particles on the training data used for cost calculations.
+        :param particles: The particles of size (M, J).
+        :return: The untransformed predictions of size (N, J).
+        """
+        return torch.zeros((10, particles.shape[1]))
+
+    def calculate_energy_potential(
+        self, particles: torch.Tensor, cost: torch.Tensor
+    ) -> float:
+        """
+        Calculates the energy potential of the particles.
+        :param particles: Particles of size (M, J).
+        :param cost: The cost of size (J,).
+        :return: The average energy potential of the particles.
+        """
+        return 0.0
+
+    def _calculate_particle_update(
+        self,
+        particles: torch.Tensor,
+        cost_derivative: torch.Tensor,
+        step_size: float,
+    ) -> torch.Tensor:
+        """
+        Calculates the update for each particle following the Projected Langevin sampling.
+        :param particles: Particles of size (M, J).
+        :param cost_derivative: The derivative of the cost function of size (N, J).
+        :param step_size: A step size for the projected Langevin sampling update in the form of a scalar.
+        :return: The update to be applied to the particles of size (M, J).
+        """
+        return torch.zeros_like(particles)
+
+    def sample_predictive_noise(
+        self,
+        particles: torch.Tensor,
+        x: torch.Tensor,
+    ):
+        """
+        Samples the predictive noise for a given input.
+        :param particles: Particles of size (M, J)
+        :param x: Test points of size (N*, D)
+        :return: The predictive noise of size (N*, J)
+        """
+        return torch.zeros((x.shape[0], particles.shape[1]))
+
+    def predict_untransformed_samples(
+        self,
+        particles: torch.Tensor,
+        x: torch.Tensor,
+        noise: torch.Tensor = None,
+    ) -> torch.Tensor:
+        """
+        Predicts samples for given test points x without applying the output transformation.
+        :param particles: Particles of size (M, J).
+        :param x: Test points of size (N*, D).
+        :param noise: A noise tensor of size (N*, J), if None, it is sampled from the predictive noise distribution.
+        :return: Predicted samples of size (N*, J).
+        """
+        return torch.zeros((x.shape[0], particles.shape[1]))
---
+++ mockers/cost.py
@@ -0,0 +1,46 @@
+import torch
+
+from src.projected_langevin_sampling.costs.base import PLSCost
+
+
+class MockCost(PLSCost):
+    """
+    A mock cost used for testing projected Langevin sampling.
+
+    N is the number of training points.
+    M is the dimensionality of the function space approximation.
+    J is the number of particles.
+    D is the dimensionality of the data.
+    """
+
+    def predict(
+        self, prediction_samples: torch.Tensor
+    ) -> torch.distributions.Distribution:
+        """
+        Constructs the predictive distribution from the prediction samples.
+        :param prediction_samples: The prediction samples of size (N, J).
+        :return: The predictive distribution.
+        """
+        return torch.distributions.Normal(0, 1)
+
+    def calculate_cost(
+        self, untransformed_train_prediction_samples: torch.Tensor
+    ) -> torch.Tensor:
+        """
+        Calculates the cost current particles. This method takes the untransformed train prediction
+        samples calculated with the current particles. This is implemented in the basis class of PLS.
+        :param untransformed_train_prediction_samples: The untransformed train prediction samples of size (N, J).
+        :return: The cost of size (J,) for each particle.
+        """
+        return torch.zeros(untransformed_train_prediction_samples.shape[1])
+
+    def calculate_cost_derivative(
+        self, untransformed_train_prediction_samples: torch.Tensor
+    ) -> torch.Tensor:
+        """
+        Calculates the cost derivative of the untransformed train prediction samples. These are the prediction samples
+        before being transformed by the link function.
+        :param untransformed_train_prediction_samples: The untransformed train prediction samples of size (N, J).
+        :return: The cost derivative of size (N, J).
+        """
+        return torch.zeros_like(untransformed_train_prediction_samples)
